ad83b025975ab9d808f5c6c4910b574101340b12
Hikmethan KOLAY <hikmethan.kolay@gmail.com>
2025-02-06 16:10:56 +0300

    Update UpdateRequestDTO.java

--- src/main/java/com/hikmethankolay/user_auth_system/dto/UpdateRequestDTO.java
+++ src/main/java/com/hikmethankolay/user_auth_system/dto/UpdateRequestDTO.java
@@ -4,5 +4,5 @@ import com.hikmethankolay.user_auth_system.entity.Role;
 
 import java.util.Set;
 
-public record UpdateUserDTO(String username, String email, String password, Set<Role> roles) {
+public record UpdateRequestDTO(String username, String email, String password, Set<Role> roles) {
 }
